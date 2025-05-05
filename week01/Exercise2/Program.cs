ac2727d99d557b34dfca897750f2da3d578da4ef
Levi Omagbemi <leviomagbemi15@gmail.com>
2025-05-05 13:50:05 +0100

    Completed exercise 2 - grade detector program

--- week01/Exercise2/Program.cs
+++ week01/Exercise2/Program.cs
@@ -4,6 +4,58 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise2 Project.");
+        string letter;
+        string sign;
+        
+        // Ask user for grade percentage
+        // And convert grade to number
+        Console.Write("What is your grade percentage? ");
+        string userInput = Console.ReadLine();
+        int grade = int.Parse(userInput);
+        
+        // Get last digit of grade from userInput String
+        // Convert from char data type to string
+        // Convert from string to number
+        string lastValue = userInput[1].ToString();
+        int lastdigit = int.Parse(lastValue);
+        
+        // Check grade percentage and assign a Letter
+        if(grade >= 90)
+        {
+            letter = "A";
+        }
+        else if (grade >= 80)
+        {
+            letter = "B";
+        }
+        else if (grade >= 70)
+        {
+            letter = "C";
+        }
+        else if (grade >= 60)
+        {
+            letter = "D";
+        }
+        else
+        {
+            letter = "F";
+        }
+
+        // Check grade last digit and assign a sign
+        if(lastdigit >= 7 && letter != "A")
+        {
+            sign = "+";
+        }
+        else if (lastdigit < 3 && letter != "F" )
+        {
+            sign = "-";
+        }
+        else 
+        {
+            sign = "";
+        }
+
+        // Display output in console
+        Console.WriteLine(sign + letter);
     }
 }
