2bd9b2f66487b3e6a1e28165b97d4396759f8012
Levi Omagbemi <leviomagbemi15@gmail.com>
2025-05-07 15:09:22 +0100

    Exercise 3 - Guess my number game

--- week01/Exercise3/Program.cs
+++ week01/Exercise3/Program.cs
@@ -1,12 +1,45 @@
 using System;
+using System.Runtime.InteropServices;
 
 class Program
 {
     static void Main(string[] args)
     {
-        int magicNumber;
-        int guessNumber;
+        string playAgain = "yes";
 
-        while
+        while(playAgain == "yes")
+        {
+            int magicNumber;
+            int guessNumber;
+            int guessCount = 0;
+
+            do
+            {
+                Random randomGenerator = new Random();
+                magicNumber = randomGenerator.Next(1, 101);
+                Console.WriteLine(magicNumber);
+
+                Console.Write("What is your guess number? ");
+                guessNumber = int.Parse(Console.ReadLine());
+
+                guessCount += 1;
+
+                if (magicNumber > guessNumber)
+                {
+                    Console.WriteLine("Guess higher");
+                }
+                else if (magicNumber < guessNumber)
+                {
+                    Console.WriteLine("Guess lower");
+                }
+                else
+                {
+                    Console.WriteLine($"You guessed it! You made {guessCount} guesses");
+
+                    Console.Write("Do you want to continue playing(Yes/No)? ");
+                    playAgain = Console.ReadLine().ToLower();
+                }
+            } while(magicNumber != guessNumber);
+        }
     }
 }
