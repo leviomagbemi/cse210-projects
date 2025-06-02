49059ff68aa5cca36bc701d2de5947aabd128554
Levi Omagbemi <leviomagbemi15@gmail.com>
2025-06-02 10:45:57 +0100

    Week05 -  Homework Project

--- week05/Homework/Program.cs
+++ week05/Homework/Program.cs
@@ -4,6 +4,24 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Homework Project.");
+        Assignment assignment = new Assignment("Levi Omagbemi", "Inheritance application");
+        Console.WriteLine(assignment.GetSummary());
+
+        MathAssignment mathAssignment = new MathAssignment(
+            "Levi Omagbemi",
+            "Algebra",
+            "7.3",
+            "8-13"
+            );
+        Console.WriteLine(mathAssignment.GetSummary());
+        Console.WriteLine(mathAssignment.GetHomeworkList());
+
+        WritingAssignment writingAssignment = new WritingAssignment(
+            "Levi Omagbemi",
+            "Nigerian History",
+            "The Nigerian Civil War (1967–1970)"
+        );
+        Console.WriteLine(writingAssignment.GetSummary());
+        Console.WriteLine(writingAssignment.GetWritingInformation());
     }
 }
