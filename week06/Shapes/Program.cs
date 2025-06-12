33d1f30c6aae3e135cd468772c6cff1c12cdc1db
Levi Omagbemi <leviomagbemi15@gmail.com>
2025-06-12 14:40:31 +0100

    Week06 Polymorphism - Shapes

--- week06/Shapes/Program.cs
+++ week06/Shapes/Program.cs
@@ -4,6 +4,36 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Shapes Project.");
+        Square square = new Square("red", 4);
+        // string squareColor = square.GetColor();
+        // double squareArea = square.GetArea();
+        // Console.WriteLine($"Square color: {squareColor}, Square area: {squareArea}");
+
+
+        Rectangle rectangle = new Rectangle("yellow", 4, 5);
+        // string rectangleColor = rectangle.GetColor();
+        // double rectangleArea = rectangle.GetArea();
+        // Console.WriteLine($"Rectangle color: {rectangleColor}, Rectangle area: {rectangleArea}");
+
+
+        Circle circle = new Circle("blue", 25);
+        // string circleColor = circle.GetColor();
+        // double circleArea = circle.GetArea();
+        // Console.WriteLine($"Circle color: {circleColor}, Circle area: {circleArea}");
+
+
+        List<Shape> shapes = new List<Shape>();
+        shapes.Add(square);
+        shapes.Add(rectangle);
+        shapes.Add(circle);
+
+        foreach (Shape shape in shapes)
+        {
+            string color = shape.GetColor();
+            double area = shape.GetArea();
+
+            Console.WriteLine($"Color: {color}");
+            Console.WriteLine($"Area: {area}");
+        }
     }
 }
